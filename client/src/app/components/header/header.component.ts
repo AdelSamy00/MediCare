892eee0a4932210c2d54629a754e9ccbd19c4f4b
Adel Samy <119124469+AdelSamy00@users.noreply.github.com>
2025-04-02 14:37:01 +0200

    Configure translation services and ensure the header is translated.

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MyTranslateService } from '../../core/services/my-translate.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  imports: [RouterLink],
+  imports: [RouterLink,TranslateModule],
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  readonly _MyTranslateService:MyTranslateService = inject(MyTranslateService);
+  readonly _TranslateService:TranslateService = inject(TranslateService);
 
   ngOnInit() {
   }
-
+  change(event:Event):void{
+    /* console.log((event.target as HTMLSelectElement).value); */
+    let lang = (event.target as HTMLSelectElement).value;
+    this._MyTranslateService.changeLang(lang);
+  }
 }
---
+++ client/src/app/core/services/my-translate.service.ts
@@ -0,0 +1,42 @@
+import { isPlatformBrowser } from '@angular/common';
+import { inject, Injectable, PLATFORM_ID, Renderer2, RendererFactory2 } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyTranslateService {
+
+  private readonly _TranslateService: TranslateService = inject(TranslateService);
+  private readonly _PLATFORM_ID: Injectable = inject(PLATFORM_ID)
+  private readonly _Renderer2: Renderer2 = inject(RendererFactory2).createRenderer(null,null)
+
+  constructor() {
+    if(isPlatformBrowser(this._PLATFORM_ID)){
+      this._TranslateService.setDefaultLang('en');
+      this.setLang();
+    }
+  }
+
+  setLang() {
+    if(isPlatformBrowser(this._PLATFORM_ID)){
+      let lang = localStorage.getItem('lang');
+      if(lang === null) lang = 'en';
+      this._TranslateService.use(lang !);
+      if(lang === 'en'){
+        this._Renderer2.setAttribute(document.documentElement,'dir','ltr');
+      this._Renderer2.setAttribute(document.documentElement,'lang','en');
+      }else if(lang === 'ar'){
+        this._Renderer2.setAttribute(document.documentElement,'dir','rtl');
+      this._Renderer2.setAttribute(document.documentElement,'lang','ar');
+      } 
+    }
+  }
+
+  changeLang(lang:string){
+    if(isPlatformBrowser(this._PLATFORM_ID)){
+      localStorage.setItem('lang',lang);
+      this.setLang();
+    }
+  }
+}
