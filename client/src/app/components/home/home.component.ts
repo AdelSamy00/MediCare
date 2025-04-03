42d6ca527f9e4c94ff4fb0c5829639d70637e273
Adel Samy <119124469+AdelSamy00@users.noreply.github.com>
2025-04-03 11:45:53 +0200

    Add a dynamic image slider to the home page for enhanced user experience.

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
-
+import { CarouselModule,OwlOptions  } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ CarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  
+
+  customOptions: OwlOptions = {
+    rewind: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    navSpeed: 700,
+    items: 1,
+    nav: false,
+    rtl: true,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+  }
+  images = [
+    { id: '1', url: 'slider1.jpg' },
+    { id: '2', url: 'slider2.jpg' },
+    { id: '3', url: 'slider3.jpg' },
+    { id: '4', url: 'slider4.jpg' }
+  ];
 }
